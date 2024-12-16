fe79e128ce122459c8da3af8dc80edf305b77325
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-12-16 13:29:17 -0300

    fix: grpc

--- serverJava/src/main/java/api/config/GrpcConfig.java
+++ serverJava/src/main/java/api/config/GrpcConfig.java
@@ -1,9 +1,11 @@
 package api.config;
 
 import api.application.services.CreatorGrpcService;
+import api.application.services.SubscriptionGrpcService;
 import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -12,15 +14,17 @@ import java.io.IOException;
 public class GrpcConfig {
     private final Server server;
 
-    public GrpcConfig(CreatorGrpcService creatorGrpcService) {
+    public GrpcConfig(CreatorGrpcService creatorGrpcService, SubscriptionGrpcService subscriptionGrpcService) {
         this.server = ServerBuilder.forPort(50051)
                 .addService((BindableService) creatorGrpcService)
+                .addService((BindableService) subscriptionGrpcService)
+                .addService(ProtoReflectionService.newInstance())
                 .build();
     }
 
     public void start() throws IOException {
         server.start();
-        System.out.println("gRPC Server started on port 50051");
+        System.out.println("gRPC Server started on port 50051 with Reflection enabled.");
         Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
     }
 
@@ -28,4 +32,3 @@ public class GrpcConfig {
         server.awaitTermination();
     }
 }
-
